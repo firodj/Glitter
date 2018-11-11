ed89c33ae1bd8ba63e719ab101092d82601aad18
Fadhil Mandaga <firodj@gmail.com>
2018-11-11 21:46:34 +0700

    Add ImGui

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -4,17 +4,31 @@
 // System Headers
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+
+static const char* glsl_version = "#version 330 core";
+static int mScreenWidth = 0;
+static int mScreenHeight = 0;
 
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW!" << std::endl;
+        return EXIT_FAILURE;
+    }
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    std::cout << "Running against GLFW " << major << "." << minor << "." << revision << std::endl;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
@@ -22,27 +36,70 @@ int main(int argc, char * argv[]) {
 
     // Check for Valid Context
     if (mWindow == nullptr) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
+        std::cerr << "Failed to Create OpenGL Context" << std::endl;
         return EXIT_FAILURE;
     }
 
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
     gladLoadGL();
-    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+    // Setup Dear ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+    ImGui_ImplGlfw_InitForOpenGL(mWindow, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Setup style
+    ImGui::StyleColorsDark();
+
+    glfwGetFramebufferSize(mWindow, &mScreenWidth, &mScreenHeight);
+    glViewport(0, 0, mScreenWidth, mScreenHeight);
 
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
+        glfwPollEvents();
+
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, true);
 
+	// Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            static bool show_demo_window = true;
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window); 
+        }
+        ImGui::Render();
+    
+        glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
         // Background Fill Color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // Draw something
+
+        // Draw ImGui
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
-        glfwPollEvents();
-    }   glfwTerminate();
+    }   
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(mWindow);
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
